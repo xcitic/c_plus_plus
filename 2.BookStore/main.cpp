f2439d5dbe4b421884d7300b8c223a21471c8b86
xcitic <xcitic@gmail.com>
2020-03-17 22:33:00 +0100

    Extending class and passing in an object to array

--- 2.BookStore/main.cpp
+++ 2.BookStore/main.cpp
@@ -35,6 +35,16 @@ class Book {
 };
 
 
+class BookShelf: public Book {
+    public:
+        Book books[10];
+
+        void add_book_to_shelf(Book newBook) {
+            books[0] = newBook;
+        }
+};
+
+
 int main()
 {
     Book book1;
@@ -42,10 +52,17 @@ int main()
     book1.set_author("Isac Asimov");
     book1.set_pages(250);
 
+    BookShelf shelf;
+    shelf.add_book_to_shelf(book1);
+
+    string book1_title = shelf.books[0].get_title();
+    string book1_author = shelf.books[0].get_author();
+    int book1_pages = shelf.books[0].get_pages();
+
     cout << "Book 1:" << endl;
-    cout << "Title: " << book1.get_title() << endl;
-    cout << "Author: " << book1.get_author() << endl;
-    cout << "Pages: " << book1.get_pages() << endl;
+    cout << "Title: " << book1_title << endl;
+    cout << "Author: " << book1_author << endl;
+    cout << "Pages: " << book1_pages << endl;
 
     return 0;
 }
